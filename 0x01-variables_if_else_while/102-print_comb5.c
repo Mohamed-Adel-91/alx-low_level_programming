ada3b3e3a8945764c56f6eecca3d77bc3ffde0c6
Mohamed Adel <mohamed101291@gmail.com>
2023-06-16 13:59:22 -0700

    C_variables_if_else_while

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descripstion: Function from 0 to 99
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int d1 = 0;
+	int d2;
+
+	while (d1 <= 99)
+	{
+		d2 = d1;
+
+		while (d2 <= 99)
+		{
+			if (d2 != d1)
+			{
+				putchar((d1 / 10) + 48);
+				putchar((d1 % 10) + 48);
+				putchar(' ');
+				putchar((d2 / 10) + 48);
+				putchar((d2 % 10) + 48);
+
+				if (d1 != 98 || d2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			d2++;
+		}
+		d1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
