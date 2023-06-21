4dbc089ab4ebc71a13caab8f7fcef5f77c0f40d9
Mohamed Adel <mohamed101291@gmail.com>
2023-06-20 19:24:01 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,14 +20,14 @@ void times_table(void)
 			_putchar(',');
 			_putchar(' ');
 
-			pord = num * mult;
+			prod = num * mult;
 
-			if (pord <= 9)
+			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putchar((pord / 10) + 48);
+				_putchar((prod / 10) + 48);
 
-			_putchar((pord % 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
