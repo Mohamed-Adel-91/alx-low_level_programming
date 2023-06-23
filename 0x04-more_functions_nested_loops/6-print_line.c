31250ebee9a9c4346b6342a3944c86bac220ee19
Mohamed Adel <mohamed101291@gmail.com>
2023-06-22 19:37:12 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,13 +8,13 @@
 
 void print_line(int n)
 {
-	int ch;
+	int lnChr;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (ch = 1; ch <= n; ch++)
+		for (lnChr = 1; lnChr <= n; lnChr++)
 			_putchar('_');
 		_putchar('\n');
 	}
