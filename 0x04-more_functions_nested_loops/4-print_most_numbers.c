9c3504a2d95e4881facf25b4df176eb8078cdf68
Mohamed Adel <mohamed101291@gmail.com>
2023-06-22 19:18:21 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,8 @@
 
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
- *
+ * Do not print 2 and 4
+ * can only use _putchar twice in your code
  * Return: Always 0
  */
 
@@ -16,4 +17,5 @@ void print_most_numbers(void)
 			continue;
 		_putchar(num + 48);
 	}
+	_putchar('\n');
 }
