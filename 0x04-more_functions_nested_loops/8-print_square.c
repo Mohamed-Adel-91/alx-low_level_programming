01953eb284844e82fe9fdc25bd5eeaae4a74a9b1
Mohamed Adel <mohamed101291@gmail.com>
2023-06-23 09:49:56 -0700

    more_functions_nested_loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,12 @@
 
 void print_square(int size)
 {
-  int row, col;
+	int row, col;
 
-  for (row = 1; row <= size; row++)
-  {
-    for (col = 1; col <= size; col++)
-      _putchar('#');
-    _putchar('\n');
-  }
+	for (row = 1; row <= size; row++)
+	{
+	for (col = 1; col <= size; col++)
+		_putchar('#');
+	_putchar('\n');
+	}
 }
