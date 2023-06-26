0f1aa709fb0e10153f7dbc3259267a5c7312d5b9
Mohamed Adel <mohamed101291@gmail.com>
2023-06-26 09:02:54 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generate password for file 101-crackme
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int sum;
+	char ch;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += ch;
+		putchar(ch);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
