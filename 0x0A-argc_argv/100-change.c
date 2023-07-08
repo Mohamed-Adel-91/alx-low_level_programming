892fffbf47958095d165bcc78f6ad0b318903910
Mohamed Adel <mohamed101291@gmail.com>
2023-07-07 18:30:24 -0700

    argc_argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,31 +11,31 @@
 
 int main(int argc, char *argv[])
 {
-        int cent, money = 0;
+	int cent, money = 0;
 
-        if (argc != 2)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        else
-        {
-                cent = atoi(argv[1]);
-                while (cent > 0)
-                {
-                        if (cent >= 25)
-                                cent -= 25;
-                        else if (cent >= 10)
-                                cent -= 10;
-                        else if (cent >= 5)
-                                cent -= 5;
-                        else if (cent >= 2)
-                                cent -= 2;
-                        else if (cent >= 1)
-                                cent -= 1;
-                        money += 1;
-                }
-        }
-        printf("%d\n", money);
-        return (0);
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		cent = atoi(argv[1]);
+		while (cent > 0)
+		{
+			if (cent >= 25)
+				cent -= 25;
+			else if (cent >= 10)
+				cent -= 10;
+			else if (cent >= 5)
+				cent -= 5;
+			else if (cent >= 2)
+				cent -= 2;
+			else if (cent >= 1)
+				cent -= 1;
+			money += 1;
+		}
+	}
+	printf("%d\n", money);
+	return (0);
 }
