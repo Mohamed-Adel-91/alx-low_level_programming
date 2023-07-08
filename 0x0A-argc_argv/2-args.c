2789abb67e4c327e0bd7c27f35bb7d7795773e00
MohamedAdel <mohamed101291@gmail.com>
2023-07-08 03:25:41 +0300

    argc, argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,18 +3,16 @@
 
 /**
  * main - a program that prints all arguments it receives
- *
  * @argc: integer of arguments passed
  * @argv: list of pointer arguments passed
- *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
 {
-	int i;
+        int i;
 
-	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[i]);
-	return (0);
+        for (i = 0; i < argc; i++)
+                printf("%s\n", argv[i]);
+        return (0);
 }
