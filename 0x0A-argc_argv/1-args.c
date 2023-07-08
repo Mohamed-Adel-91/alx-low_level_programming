3ce5a9780a31f18cb13476d24015cb1dc6b374b2
MohamedAdel <mohamed101291@gmail.com>
2023-07-08 03:24:33 +0300

    argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * main - a program that prints the number of arguments passed into it
  * @argc: integer of arguments passed
  * @argv: list of pointer of arguments passed
- * Return: Always 0
+ * Return: 0
  */
 
 int main(int argc, char const *argv[])
