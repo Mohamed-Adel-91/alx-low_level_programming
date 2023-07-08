9b61b9070b3121f0e13a83e4ecc4fa9f620af0ca
MohamedAdel <mohamed101291@gmail.com>
2023-07-08 03:28:23 +0300

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,14 +12,14 @@
 
 int isNum(char num[])
 {
-	int i, j = strlen(num);
+        int i, j = strlen(num);
 
-	for (i = 0; i < j; i++)
-	{
-		if (!isdigit(num[i]))
-			return (1);
-	}
-	return (0);
+        for (i = 0; i < j; i++)
+        {
+                if (!isdigit(num[i]))
+                        return (1);
+        }
+        return (0);
 }
 
 /**
@@ -31,28 +31,28 @@ int isNum(char num[])
 
 int main(int argc, char *argv[])
 {
-	int i, s;
+        int i, s;
 
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-	else
-	{
-		s = 0;
-		for (i = 1; i < argc; i++)
-		{
-			if (isNum(argv[i]) == 0)
-			{
-				s += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", s);
-	}
-	return (0);
+        if (argc == 1)
+        {
+                printf("0\n");
+        }
+        else
+        {
+                s = 0;
+                for (i = 1; i < argc; i++)
+                {
+                        if (isNum(argv[i]) == 0)
+                        {
+                                s += atoi(argv[i]);
+                        }
+                        else
+                        {
+                                printf("Error\n");
+                                return (1);
+                        }
+                }
+                printf("%d\n", s);
+        }
+        return (0);
 }
