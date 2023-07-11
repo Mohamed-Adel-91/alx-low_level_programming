34d9074c27fbd8d8c35dc773ed07980af89a99dc
Mohamed Adel <mohamed101291@gmail.com>
2023-07-11 14:45:44 -0700

    malloc_free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * *_strdup - a function that duplicates a string
+ *
+ * @str: input string
+ *
+ * Return: string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0, size = 0;
+	char *string;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[size] != '\0')
+		size++;
+	string = malloc((size + 1) * sizeof(*str) + 1);
+	if (string == NULL)
+		return (NULL);
+	while (str[i] != '\0')
+	{
+		string[i] = str[i];
+		i++;
+	}
+	return (string);
+	free(string);
+}
